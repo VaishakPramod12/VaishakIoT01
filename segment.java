8780f85e2d790335464073e58d85e7fa1e0805a6
VaishakPramod12 <vaishakpramod2006@gmail.com>
2026-08-29 16:30:14 +0530

    Add files via upload

---
+++ segment.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class segment {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {12, 25, 32, 45, 52, 67};
+
+        int search = 2;
+
+        ArrayList<Integer> result = new ArrayList<>();
+
+        for (int number : numbers) {
+
+            if (String.valueOf(number).contains(String.valueOf(search))) {
+                result.add(number);
+            }
+        }
+
+        System.out.println("Search results:");
+
+        for (int number : result) {
+            System.out.println(number);
+        }
+    }
+}
+
---
+++ sumnodes.java
@@ -0,0 +1,10 @@
+
+public class sumnodes 
+{
+static class node
+{
+	int data;
+	node left, right;
+	
+}
+}
